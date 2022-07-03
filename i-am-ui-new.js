7644e42f0685969d2af57bc9e74ce719f1f77165
zturtledog <71353802+zturtledog@users.noreply.github.com>
2022-07-03 11:29:06 -0700

    Update i-am-ui-new.js

--- i-am-ui-new.js
+++ i-am-ui-new.js
@@ -1,8 +1,5 @@
 function button(x, y, w, h) {//a inline button like the first version
   if (hitbox(x, y, w, h, mouseX, mouseY)) {
-    noStroke();
-    fill("#ffffff22");
-    rect(x, y, w, h);
 
     if (mouseIsPressed) {
       return true;
@@ -15,11 +12,6 @@ function button(x, y, w, h) {//a inline button like the first version
 
 function vslider(v, x, y, h, m, f) {//a verticle slider
   let uy = map(v, m, f, y, y + h);
-
-  noFill();
-  line(x, y, x, uy - 5);
-  line(x, y + h, x, uy + 5);
-  rect(x - 2, uy - 5, 4, 10, 5);
 
   if (hitbox(x - 5, y, 10, h, mouseX, mouseY)) {
     if (mouseIsPressed) {
