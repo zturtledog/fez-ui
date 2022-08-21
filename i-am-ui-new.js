98a5b65eb98c1a8025c5980ea8e8cca4391a6cb0
zturtledog <71353802+zturtledog@users.noreply.github.com>
2022-08-21 01:50:16 -0700

    fixed some bugs added features

--- i-am-ui-new.js
+++ i-am-ui-new.js
@@ -1,41 +1,32 @@
-function button(x, y, w, h) {//a inline button like the first version
-  if (hitbox(x, y, w, h, mouseX, mouseY)) {
+// localizations
+let keys = "`1234567890-=\tqwertyuiop[]\\asdfghjkl;'zxcvbnm,./ ~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?"
 
-    if (mouseIsPressed) {
-      return true;
-    }
+//permance
 
-    cursor(HAND);
-  }
-  return false;
-}
-
-function vslider(v, x, y, h, m, f) {//a verticle slider
-  let uy = map(v, m, f, y, y + h);
-
-  if (hitbox(x - 5, y, 10, h, mouseX, mouseY)) {
-    if (mouseIsPressed) {
-      return map(mouseY, y, y + h, m, f);
-    }
-  }
-
-  return v;
-}
-
-function slider() {}
-
-function checkbox() {}
-
-function colorpicker() {}
+let pdown
 
 //util
 
-function centredtext(_text, x, y) {
-  text(_text, x - textWidth(_text) / 2, y);
-}
-
 function hitbox(x, y, w, h, px, py) {
   return px > x && px < x + w && py > y && py < y + h;
+}
+
+function iiil(t,l) {
+  for (let i of l) {
+    if (t == i)
+      return true
+  }
+  return false
+}
+
+function gridlock(g,m) {
+  return round(g/m)*m
+}
+
+//shape
+
+function centredtext(_text, x, y) {
+  text(_text, x - textWidth(_text) / 2, y);
 }
 
 function hyperect(x,y,w,h,c) {
@@ -49,4 +40,259 @@ function hyperect(x,y,w,h,c) {
   vertex(x, y+h-c);
   vertex(x, y+c);
   endShape(CLOSE);
+}
+
+//general
+
+function uiupd() {
+  pdown = mouseIsPressed
+}
+
+//function
+
+function button(x, y, w, h) {
+  if (hitbox(x, y, w, h, mouseX, mouseY)) {
+    noStroke();
+    fill("#dddddd44");
+    rect(x, y, w, h, 5);
+    cursor(HAND);
+  }
+
+  if (
+    hitbox(x, y, w, h, mouseX, mouseY) &&
+    mouseIsPressed &&
+    pdown != mouseIsPressed
+  ) {
+    return true;
+  }
+
+  return false;
+}
+
+function checkbox(v,x,y,w,h,r) {
+  if (!r) {
+    rect(x,y,w,h,5)
+    text(v?"⊙":"⊗",x+(w/2)-9,y+(h/2)+7)
+  }
+  
+  if (button(x,y,w,h)) {
+    return !v
+  }
+  
+  return v
+}
+
+function vslider(v, x, y, h, m, f, r, g) {
+  let uy = map(v, m, f, y, y + h);
+
+  if (!r) {noFill();
+  line(x, y, x, uy - 5);
+  line(x, y + h, x, uy + 5);
+  rect(x - 2, uy - 5, 4, 10, 5);}
+
+  if (hitbox(x - 5, y, 10, h, mouseX, mouseY)) {
+    cursor("grab")
+    if (mouseIsPressed) {
+      cursor("grabbing")
+      if (keyIsDown(17) && g) {
+        return gridlock(map(mouseY, y, y + h, m, f),g);
+      }
+      return map(mouseY, y, y + h, m, f);
+    }
+  }
+
+  return v;
+}
+
+function slider(v, x, y, w, m, f,r,g) {
+  let ux = map(v, m, f, x, x+w);
+
+  if (!r) {noFill();
+  line(x, y, ux - 5, y);
+  line(x + w, y, ux + 5, y);
+  rect(ux - 5,y-2, 10, 4, 5);}
+
+  if (hitbox(x, y - 5, w, 10, mouseX, mouseY)) {
+    cursor("grab")
+    if (mouseIsPressed) {
+      cursor("grabbing")
+      if (keyIsDown(17) && g) {
+        return gridlock(map(mouseX, x, x + w, m, f),g);
+      }
+      return map(mouseX, x, x + w, m, f);
+    }
+  }
+
+  return v;
+}
+
+function slider2d(v, x, y, w, h, mw, fw, mh, fh, r, g) {
+  let ux = map(v.x, mw, fw, x, x+w);
+  let uy = map(v.y, mh, fh, y, y+h);
+
+  if (!r) {
+    noFill();
+    rect(x,y,w,h,5)
+    line(x,uy+5,ux,uy+5)
+    line(x+w,uy+5,ux+10,uy+5)
+    line(ux+5,y,ux+5,uy)
+    line(ux+5,y+h,ux+5,uy+10)
+    rect(ux,uy, 10, 10, 3);
+  }
+
+  if (hitbox(x, y, w, h, mouseX, mouseY)) {
+    cursor("grab")
+    if (mouseIsPressed) {
+      cursor("grabbing")
+      if (keyIsDown(17) && g) {
+        return {
+          x:gridlock(map(mouseX-5, x, x + w, mw, fw),g),
+          y:gridlock(map(mouseY-5, y, y + h, mh, fh),g)
+        };
+      }
+      return {
+        x:map(mouseX-5, x, x + w, mw, fw),
+        y:map(mouseY-5, y, y + h, mh, fh)
+      };
+    }
+  }
+
+  return v;
+}
+
+//classes
+
+//@note key repermanace bug (p[a]p[s]r[a])
+//@note i think i fixed it but not sure
+class onelinetext {
+  constructor(init,x,y,s) {
+    this.text = init
+    this.x = x
+    this.y = y
+    this.s = s||20
+    this.cursor = 1
+  }
+  
+  position(x,y) {
+    this.x = x
+    this.y = y
+  }
+  
+  salad() {
+    this.text = "salad"
+    this.cursor = 0
+  }
+  
+  update() {
+    this.w = textWidth(this.text);
+    
+    //inbound cursor
+    
+    if (this.cursor < 0) {
+      this.cursor = 0;
+    }
+    if (this.cursor > this.text.length) {
+      this.cursor = this.text.length;
+    }
+    
+    //cursor string data
+    
+    let data = [this.text.substring(0,this.cursor),this.text.substring(this.cursor)]
+    
+    if (this.select) {
+      // move cursor right
+      if (keyIsDown(39)) {
+        if (!this.cursorkeyleft || this.cursorkeyleftime > 30)
+        this.cursor++;
+        this.cursorkeyleft = true
+        this.cursorkeyleftime++
+      }else{
+        this.cursorkeyleft = false
+        this.cursorkeyleftime = 0
+      }
+      
+      //move cursor left
+      if (keyIsDown(37)) {
+        if (!this.cursorkeyright || this.cursorkeyrightime > 30)
+        this.cursor--;
+        this.cursorkeyright = true
+        this.cursorkeyrightime++
+      }else{
+        this.cursorkeyright = false
+        this.cursorkeyrightime = 0
+      }
+      
+      //delete previous char
+      if (keyIsDown(8)) {
+        if (!this.cursorkeydel || this.cursorkeydeltime > 50) {
+          this.cursor--;
+          this.text = data[0].substring(0,this.cursor)+data[1]
+        }
+        this.cursorkeydel = true
+        this.cursorkeydeltime++
+      }else{
+        this.cursorkeydel = false
+        this.cursorkeydeltime = 0
+      }
+      
+      //actial typing with timer
+      //@note this is where the problem happens
+      if (keyIsPressed) {
+        // console.log(char(keyCode))
+        if (iiil(key,keys))
+        if (!this.cursorkey || key != this.pcursorkey || this.cursorkeytime > 50) {
+          if (keyIsDown(keyCode)) {
+            this.cursor++
+            this.text = data[0]+key+data[1]
+          }        
+        }
+        this.cursorkey = true
+        this.pcursorkey = key
+        this.cursorkeytime++
+      }else{
+        this.cursorkey = false
+        this.cursorkeytime = 0
+      }
+    }
+    
+    //select and position the cursor
+    
+    if (hitbox(this.x,this.y-this.s-2.5,(this.w < 10 ? 10 : this.w),this.s+5,mouseX,mouseY)) {
+      cursor(TEXT)
+      if (mouseIsPressed) {
+        let record = "";
+        for(let i = 0;i < this.text.length; i++) {
+          record += this.text.charAt(i)
+          if (hitbox(this.x,this.y-this.s-2.5,textWidth(record),this.s+5,mouseX,mouseY)) {
+            this.cursor = i+1
+            break;
+          }
+        }
+        
+        this.select = true
+      }
+    }
+    else{
+      cursor(ARROW)
+      if (mouseIsPressed) {
+        this.select = false
+      }
+    } 
+  }
+  
+  draw() {
+    let data = [this.text.substring(0,this.cursor),this.text.substring(this.cursor)]
+    
+    //draw
+    
+    noStroke()
+    textSize(this.s)
+    text(data[0],this.x,this.y)
+    text(data[1],this.x+textWidth(data[0])+1,this.y)
+    strokeWeight(2)
+    stroke(0)
+    if (this.select)
+      line(this.x+textWidth(data[0])+1,this.y,this.x+textWidth(data[0])+1,this.y-this.s+5)
+    line(this.x,this.y+2,this.x+(this.w < 10 ? 10 : this.w),this.y+2)
+  }
 }
